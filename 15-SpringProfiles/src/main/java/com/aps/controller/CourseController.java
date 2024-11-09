017a015af359b4be07dba9a843f42943132d02bf
Abhay Pratap Singh <abhaypratap.singh02@ad.infosys.com>
2024-11-09 22:01:00 +0530

    Spring profiles learned

---
+++ 15-SpringProfiles/src/main/java/com/aps/controller/CourseController.java
@@ -0,0 +1,19 @@
+package com.aps.controller;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.aps.entity.Course;
+
+@RestController
+public class CourseController {
+
+	@RequestMapping("/courses")
+	public List<Course> getCourses() {
+		return Arrays.asList(new Course(1, "book1", "author1"), new Course(2, "book2", "author2"),
+				new Course(0, "zerobook", "zeropublisher"));
+	}
+}

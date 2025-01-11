0f3212250960208de02f1f3369ad62d0c176a9b8
Abhay Pratap Singh <abhaypratap.singh02@ad.infosys.com>
2025-01-11 22:53:26 +0530

    On form submit open login's post mapping

--- 27-MultiMethodsAndViews/src/main/java/com/aps/LoginController.java
+++ 27-MultiMethodsAndViews/src/main/java/com/aps/LoginController.java
@@ -2,12 +2,24 @@ package com.aps;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class LoginController {
 
-	@RequestMapping("/login")
+//	Earlier both get and post were handled by same request
+//	We need to have different type of action
+
+	@RequestMapping(value = "/login", method = RequestMethod.GET)
 	public String gotoLoginPage() {
 		return "login";
+	}
+
+//	If only one mapping was there then you'll get 404 error as post is not present
+//	To resolve this we create post request handler
+
+	@RequestMapping(value = "/login")
+	public String goToWelcomePage() {
+		return "welcome";
 	}
 }
---
+++ 27-MultiMethodsAndViews/src/main/resources/META-INF/resources/WEB-INF/jsp/welcome.jsp
@@ -0,0 +1,8 @@
+<html>
+    <head>
+        <title>welcome page</title>
+    </head>
+    <body>
+        <div>Welcome to the site</div>
+    </body>
+</html>
